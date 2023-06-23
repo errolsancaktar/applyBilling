d9e1663cf3c375261647fbfabc8796a419100c04
Errol Sancaktar <errols@lucidpoint.io>
2023-06-23 09:47:31 -0600

    add logging

--- code/main.py
+++ code/main.py
@@ -2,25 +2,30 @@ import base64
 import os
 import json
 import functions_framework
+import logging
 from google.cloud import billing_v1
 
+
+## Apply Environment Variables ##
 billingAccount = os.environ['LP_BILLING_ACCOUNT']
 
-
+## Set Loglevel ##
+logging.setLevel("info")
 # Triggered from a message on a Cloud Pub/Sub topic.
+
 
 @functions_framework.cloud_event
 def hello_pubsub(cloud_event):
     # Print out the data from Pub/Sub, to prove that it worked
     logData = json.loads(base64.b64decode(cloud_event.data["message"]["data"]))
-    print("Log Output:")
-    print(logData)
-    print("Project ID:")
+    logging.debug("Log Output:")
+    logging.debug(logData)
+    logging.info("Project ID:")
     projectID = logData['protoPayload']['request']['project']['projectId']
-    print(projectID)
+    logging.info(projectID)
     creation_user = logData['protoPayload']['authenticationInfo']['principalEmail']
-    print("Creator:")
-    print(creation_user)
+    logging.info("Creator:")
+    logging.info(creation_user)
     fullProj = "projects/" + projectID
     if (isBillingEnabled(fullProj)):
         applyBilling(projectID, billingAccount)
@@ -33,7 +38,7 @@ def isBillingEnabled(project, billingAccount):
         name=project,
     )
     response = billingClient.get_project_billing_info(request=request)
-
+    logging.debug(response)
     return response
 
 
@@ -44,5 +49,6 @@ def applyBilling(project):
         name=project,
     )
     response = billingClient.update_billing_account(request=request)
+    logging.debug(response)
 
     return response
